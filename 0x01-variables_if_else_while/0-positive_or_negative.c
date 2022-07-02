22e51f6e2ccbd76c5cbe04252da64637c26d86ca
mikemater01 <104072911+mikemater01@users.noreply.github.com>
2022-07-02 15:11:15 +0100

    Add files via upload
    C programming alx task two

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - assigns a random number to int n
+ * for numbers > zero, n is positive
+ * for numbers < zero, n is negative
+ * for numbers == 0 n is zero
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+in this project, I will be writting some C code lines on Variables, if, else, while
