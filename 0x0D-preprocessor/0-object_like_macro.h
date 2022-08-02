e62670a241169238e1270d367a9aca99e55899c2
Mike <104072911+mikemater01@users.noreply.github.com>
2022-08-02 03:26:02 +0100

    Preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _OBJECT_LIKE_MACRO_
+#define _OBJECT_LIKE_MACRO_
+
 #define SIZE 1024
+
 #endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/*
- * main - prints the file name of the program
+/**
+ * main - prints the name of the file.
  *
- * Return: Always zero
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
