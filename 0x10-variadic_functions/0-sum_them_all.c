5c1508b2deb77a258847431af5759f95331320c6
Mike <104072911+mikemater01@users.noreply.github.com>
2022-08-05 04:14:02 +0100

    Var Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,28 @@
-/*
- * File: 0-sum_them_all.c
- * Auth: Akhivbareme
- */
-
-#include "variadic_functions.h"
 #include <stdarg.h>
-
+#include "variadic_functions.h"
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
- */
+ * sum_them_all - Function Prototype
+ * @n: Constant argument variable
+ * @...: Ellipsis
+ * Return: 0 if n == 0
+ * **/
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index, sum = 0;
+	va_list ap;
+	int sum;
+	unsigned int i;
 
-	va_start(nums, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	sum = 0;
+	va_start(ap, n);
 
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
-
-	va_end(nums);
-
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,10 @@
-#ifndef VAR_FUNCTIONS
-#define VAR_FUNCTIONS
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
 
-int _putchar(char c);
+int _putchar(char n);
 int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif
