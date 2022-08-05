c63418071a3cb8acad686160d55ca8f8951dae9c
Michael <micfem2b@gmail.com>
2022-08-04 19:44:51 -0700

    Var Function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,23 +6,26 @@
  * @n: The number of paramters passed to the function.
  * @...: A variable number of parameters
  *
- * Return: 0: If n == 0
- *         Otherwise return the sum of all parameters.
+ * Return: 0: If n = 0
+ * 		Otherwise return the sum of parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ptr;
-	unsigned int i, sum = 0;
-
-	va_start(ptr, n);
+	va_list ap;
+	int sum;
+	unsigned int i;
 
 	if (n == 0)
+	{
 		return (0);
+	}
+	sum = 0;
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ptr, int);
-
-	va_end(ptr);
-
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
 	return (sum);
 }
