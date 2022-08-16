00d1060b5a0ae879ad82da46f3dd5fd26dd3ff29
Michael <micfem2b@gmail.com>
2022-08-16 09:52:40 -0700

    task 9: Requirement fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position 
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: head of a linked list
  * @idx: index of the newly added node
  * @n: integer element
  *
- * Return: returns the address of the newesr node. NUll if false 
+ * Return: returns the address of the newesr node. NUll if false
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
