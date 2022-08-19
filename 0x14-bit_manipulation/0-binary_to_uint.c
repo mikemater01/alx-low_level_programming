7005d5c4c1fdc6f20aafce7f11457c8405c09744
Michael <micfem2b@gmail.com>
2022-08-18 17:14:28 -0700

    task 0: 0 - Requirement fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,29 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+unsigned int i, j = 0;
 
-	if (!b)
-		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-
-	return (ui);
+if (!b)
+{
+return (0);
+}
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+{
+return (0);
+}
+j = 2 * j + (b[i] - '0');
+}
+return (j);
 }
