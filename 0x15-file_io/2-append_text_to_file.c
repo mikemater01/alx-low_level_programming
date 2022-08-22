87f939966619ba587ff72f40054092103ea79079
Michael <micfem2b@gmail.com>
2022-08-22 15:41:20 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;
@@ -17,21 +17,18 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++);
+		rwr = write(fd, text_content, nletters);
+		if (rwr == -1)
+			return (-1);
+	}
 
 	close(fd);
 
