e3eafd15ce1ec056c061d4aabaf31a8639f2eb78
Michael <micfem2b@gmail.com>
2022-08-22 15:25:13 -0700

    task 2: Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,30 +10,32 @@
  *
  * Return: 1 on success and -1 on failure
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t file;
-	ssize_t b_write;
-	size_t length;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_APPEND | O_WRONLY);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
-	b_write = 0;
-	if (text_content)
-	{
-		for (length = 0; text_content[length]; length++)
-			;
+	if (!text_content)
+		text_content = "";
 
-		b_write = write(file, text_content, length);
-	}
-	close(file);
-	if (b_write == -1)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
+
+	close(fd);
 
 	return (1);
 }
