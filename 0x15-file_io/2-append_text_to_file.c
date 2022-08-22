51cf89653f72a393cc9443640f19f52d979cb1ca
Michael <micfem2b@gmail.com>
2022-08-22 15:36:18 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,10 +2,11 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: the name of the file
- * @text_content: the content of the text
+ * @filename: filename.
+ * @text_content: added content.
  *
- * Return: 1 on success and -1 on failure
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
